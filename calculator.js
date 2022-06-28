8832a0b80a44979d3b3b76758c46caf6d47e3f13
Mercy <mercyomole229@gmail.com>
2022-06-28 21:06:15 +0100

    Update

---
+++ calculator.js
@@ -0,0 +1,19 @@
+alert("CALCULATOR")
+
+const operator = prompt("Enter operator (either +, -, * or /):");
+
+let firstNumber = parseFloat(prompt("Enter First Number:"));
+let secondNumber = parseFloat(prompt("Enter second number:"));
+
+if (operator == "+") {
+    alert(result = (firstNumber + secondNumber))
+
+} else if (operator == "-") {
+    alert(result = (firstNumber - secondNumber))
+
+} else if (operator === "*") {
+    alert(result = (firstNumber * secondNumber))
+
+} else {
+    alert(result = (firstNumber / secondNumber))
+}
